4811eb60257f39d9ed4e1212945bb52f3e2de425
Than Win <than.win@gmail.com>
2019-01-26 17:55:17 +0630

    Add files via upload

---
+++ chap_two/if_block.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 22 13:01:24 2018
+
+@author: Than Win
+"""
+
+x = int(input("Please enter a number: "))
+if x < 0:
+    print('Negative Number')
+elif x == 0:
+    print('Zero')
+elif x > 0:
+    print('Positive Number')
+else:
+    print('You will never see this')
